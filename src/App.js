5366a87757bff7045c24c8d8861cfd00b64ab44c
TALLURI MANOJ KUMAR <106367643+manojtalluri12@users.noreply.github.com>
2024-02-25 17:01:07 +0530

    Dark Mode ecom

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./EcomwebSite/Navbar/NavBar";
+import Product from "./EcomwebSite/Products/Product";
+import Cart from "./EcomwebSite/CartPage/Cart";
+import Details from "./EcomwebSite/DEtails/Details";
+import Orders from "./EcomwebSite/PlaceOrder/Orders";
+import Footer from "./EcomwebSite/Footer";
+import Notfound from "./EcomwebSite/Notfound";
+import Delivery from "./EcomwebSite/PaymentGate/Delivery";
+import { EcomProvider } from "./EcomContext/EcomContext";
+import { ThemeProvider } from "./theme-context/theme-context";
+import Gotop from "./EcomwebSite/Gotop";
+
+const App = () => {
+  return (
+    <ThemeProvider>
+    <EcomProvider>
+      <NavBar/>
+      <hr/>
+      <Routes>
+        <Route path="/" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/delivery/:id" element={<Delivery />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+      <Gotop/>
+      <Footer />
+    </EcomProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
